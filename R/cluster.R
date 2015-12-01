7a101865447aa8b563f8dd232e7011446dba86d0
Volunteer <volunteer@Argus.local>
2015-12-01 13:36:28 -0500

    changes to cluster.R

--- R/cluster.R
+++ R/cluster.R
@@ -7,7 +7,7 @@ create_sparse_similarity <- function(mask, bvec, dist_thresh=max(spacing(mask)),
   mask.idx <- which(mask > 0)
   G <-  indexToCoord(mask, mask.idx)
   
-  G_knn <- FNN::get.knn(G,27)
+  G_knn <- FNN::get.knn(G,200)
   
   res <- mclapply(1:nrow(G), function(i) {
     print(i)
@@ -20,7 +20,7 @@ create_sparse_similarity <- function(mask, bvec, dist_thresh=max(spacing(mask)),
     keep <- ndist < dist_thresh
     
     vals <- series(bvec, mask.idx[i] )
-    other <- series(bvec,mask.idx[nidx[ndist < dist_thresh] ])
+    other <- series(bvec,mask.idx[nidx[keep] ])
     cv <- as.vector(simfun(vals, other))
     cbind(i, nidx[keep], cv)
     #sim <- as.vector(cor(vals,ovals))
@@ -33,7 +33,6 @@ create_sparse_similarity <- function(mask, bvec, dist_thresh=max(spacing(mask)),
   wmat <- do.call(rbind, res)
   wmat <- Matrix::sparseMatrix(dims=c(nrow(G), nrow(G)), i=wmat[,1], j=wmat[,2], x=wmat[,3])
   
-  wmat <- (wmat + t(wmat))/2
   gg <- igraph::graph.adjacency(wmat, mode="undirected", weighted=TRUE)
   
 }
