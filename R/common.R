0d59ab0751a4b36b3d416d9d39573f7e5ef8497f
Volunteer <volunteer@Argus.local>
2016-02-04 15:38:49 -0500

    labels can be continuous

--- R/common.R
+++ R/common.R
@@ -469,7 +469,7 @@ loadLabels <- function(full_design, config) {
   if (is.null(full_design[[config$label_column]])) {
     stop(paste("Error: labelColumn", config$label_column, "not found"))
   } else {
-    labels <- factor(full_design[[config$label_column]])
+    labels <- full_design[[config$label_column]]
   }
   labels
 }
@@ -478,7 +478,7 @@ loadTestLabels <- function(full_design, config) {
   if (is.null(full_design[[config$test_label_column]])) {
     stop(paste("Error: labelColumn", config$label_column, "not found"))
   } else {
-    labels <- factor(full_design[[config$test_label_column]])
+    labels <- full_design[[config$test_label_column]]
   }
   labels
 }
