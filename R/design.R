e0145074370f88a049a4b7e16d67f45e16e76a37
bbuchsbaum <brad.buchsbaum@gmail.com>
2017-04-11 07:31:23 -0400

    yolo

--- R/design.R
+++ R/design.R
@@ -10,13 +10,29 @@ y_test.mvpa_design <- function(obj) if (is.null(obj$y_test)) obj$y_train else ob
 
 
 #' @export
-mvpa_design <- function(train_design, y_train, test_design=NULL, y_test=NULL, block_var=NULL, split_formula=NULL) {
+mvpa_design <- function(train_design, y_train, test_design=NULL, y_test=NULL, block_var=NULL, split_by=NULL) {
+  
+  if (!is.null(split_by)) {
+    assert_that(purrr::is_formula(split_by))
+    split_vars <- all.vars(split_by[[2]])
+    des <- if (!is.null(test_design)) test_design else train_design
+    split_var <- do.call("interaction", lapply(vars, function(vname) as.factor(des[[vname]])))
+    minSplits <- min(table(split_var))
+    if (minSplits < 3) {
+      stop(paste("error: splitting condition results in fewer than 3 observations in at least one set"))
+    }
+    
+    split_by <- split(1:nrow(des), split_var)
+    
+  }
+  
+  
   des <- list(
     train_design=train_design,
     y_train=y_train,
     test_design=test_design,
     y_test=y_test,
-    split_formula=split_formula,
+    split_by=split_by,
     block_var=block_var
   )
   
