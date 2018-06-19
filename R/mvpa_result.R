3c8db712dd253cba8447f34badb9ec4f20e9379a
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2018-06-18 22:21:36 -0400

    docs

--- R/mvpa_result.R
+++ R/mvpa_result.R
@@ -1,20 +1,41 @@
-#' create a \code{classification_result} instance
+#' Create a \code{classification_result} instance
 #' 
-#' @param observed the observed values.
-#' @param predicted the predicted values.
+#' @param observed a vector of observed/true values.
+#' @param predicted a vector of the predicted values.
 #' @param probs a \code{matrix} of predicted probabilities, one column per level.
-#' @param testind the row indices of the test observations
+#' @param testind the row indices of the test observations.
 #' @param test_design an optional design for the test data.
 #' @param predictor an optional predictor object.
 #' @rdname classification_result
+#' 
+#' @examples 
+#' 
+#' ## a vector of observed values
+#' yobs <- factor(rep(letters[1:4], 5))
+#' 
+#' ## predicted probabilities
+#' probs <- data.frame(a=runif(1:20), b=runif(1:20), c=runif(1:20), d=runif(1:20))
+#' probs <- sweep(probs, 1, rowSums(probs), "/")
+#' 
+#' ## get the max probability per row and use this to determine the predicted class
+#' maxcol <- max.col(probs)
+#' predicted <- levels(yobs)[maxcol]
+#' 
+#' ## construct a classification result
+#' cres <- classification_result(yobs, predicted, probs)
+#' 
+#' ## compute default performance measures (Accuracy, AUC)
+#' performance(cres)
 #' @export
 classification_result <- function(observed, predicted, probs, testind=NULL, test_design=NULL,predictor=NULL) {
+  
+  
   if (is.numeric(observed)) {
     regression_result(observed, predicted, testind, test_design, predictor)
   } else if (length(levels(as.factor(observed))) == 2) {
-    binary_classification_result(observed, predicted, probs,  testind, test_design, predictor)
+    binary_classification_result(as.factor(observed), predicted, probs,  testind, test_design, predictor)
   } else if (length(levels(as.factor(observed))) > 2) {
-    multiway_classification_result(observed,predicted, probs, testind, test_design, predictor)
+    multiway_classification_result(as.factor(observed),predicted, probs, testind, test_design, predictor)
   } else {
     stop("observed data must be a factor with 2 or more levels")
   }
@@ -25,6 +46,7 @@ classification_result <- function(observed, predicted, probs, testind=NULL, test
 #' @rdname classification_result
 #' @export
 binary_classification_result <- function(observed, predicted, probs, testind=NULL, test_design=NULL, predictor=NULL) {
+  assertthat::assert_that(length(observed) == length(predicted))
   ret <- list(
     observed=observed,
     predicted=predicted,
@@ -40,13 +62,13 @@ binary_classification_result <- function(observed, predicted, probs, testind=NUL
 
 
 
-
+#' @export
 sub_result.multiway_classification_result <- function(x, indices) {
   ret <- list(
     observed=x$observed[indices],
     predicted=x$predicted[indices],
     probs=as.matrix(x$probs)[indices,],
-    testind=testind,
+    testind=testind[indices],
     test_design=x$test_design[indices,],
     predictor=x$predictor)
   
@@ -54,12 +76,13 @@ sub_result.multiway_classification_result <- function(x, indices) {
   ret
 }
 
+#' @export
 sub_result.binary_classification_result <- function(x, indices) {
   ret <- list(
     observed=x$observed[indices],
     predicted=x$predicted[indices],
     probs=as.matrix(x$probs)[indices,],
-    testind=testind,
+    testind=testind[indices],
     test_design=x$test_design[indices,],
     predictor=x$predictor)
   
@@ -73,6 +96,7 @@ sub_result.binary_classification_result <- function(x, indices) {
 #' @rdname classification_result
 #' @export
 multiway_classification_result <- function(observed, predicted, probs,testind=NULL, test_design=NULL, predictor=NULL) {
+  assertthat::assert_that(length(observed) == length(predicted))
   ret <- list(
     observed=observed,
     predicted=predicted,
