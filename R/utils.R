55292c83a0c722283c5005068100b264d1af51d5
bbuchsbaum <brad.buchsbaum@gmail.com>
2021-05-27 11:11:34 -0400

    update coalesce_join

--- R/utils.R
+++ R/utils.R
@@ -67,6 +67,33 @@ removeZeroVarianceColumns <- function(M) {
   }
 }
 
+## dfferent version
+## https://alistaire.rbind.io/blog/coalescing-joins/
+coalesce_join2 <- function(x, y, 
+                          by = NULL, suffix = c(".x", ".y"), 
+                          join = dplyr::full_join, ...) {
+  joined <- join(x, y, by = by, suffix = suffix, ...)
+  # names of desired output
+  cols <- union(names(x), names(y))
+  
+  to_coalesce <- names(joined)[!names(joined) %in% cols]
+  suffix_used <- suffix[ifelse(endsWith(to_coalesce, suffix[1]), 1, 2)]
+  # remove suffixes and deduplicate
+  to_coalesce <- unique(substr(
+    to_coalesce, 
+    1, 
+    nchar(to_coalesce) - nchar(suffix_used)
+  ))
+  
+  coalesced <- purrr::map_dfc(to_coalesce, ~dplyr::coalesce(
+    joined[[paste0(.x, suffix[1])]], 
+    joined[[paste0(.x, suffix[2])]]
+  ))
+  names(coalesced) <- to_coalesce
+  
+  dplyr::bind_cols(joined, coalesced)[cols]
+}
+
 
 coalesce_join <- function(x, y, 
                           by = NULL, suffix = c(".x", ".y"), 
@@ -76,6 +103,12 @@ coalesce_join <- function(x, y,
   cols <- union(names(x), names(y))
   
   to_coalesce <- names(joined)[!names(joined) %in% cols]
+  
+  if (length(to_coalesce) == 0) {
+    ## nothing to coalesce...
+    return(joined)
+  }
+  
   suffix_used <- suffix[ifelse(endsWith(to_coalesce, suffix[1]), 1, 2)]
   # remove suffixes and deduplicate
   to_coalesce <- unique(substr(
